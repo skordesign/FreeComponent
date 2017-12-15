9e4c4fae6a86084bb66a71f8fccd343dcfa9c2b3
skordesign <skordesign@outlook.com>
2017-12-15 17:11:16 +0700

    update rating

--- ClientApp/app/components/ratingBar/ratingBar.component.ts
+++ ClientApp/app/components/ratingBar/ratingBar.component.ts
@@ -3,23 +3,24 @@ import { Component, Output, Input } from "@angular/core";
 @Component({
     selector: 'app-rating-bar',
     templateUrl: './ratingBar.component.html',
-    styles:[`
+    styles: [`
         .fill{
             background: black;
         }
     `]
 })
 export class RatingBarComponent {
-    @Input() rating: number = 3;
+    textRating = ["Very Bad", "Bad", "So so", "Good", "Perfect"]
+    @Input() rating: number = 0;
     @Input() max: number = 5;
-    range(star:number){
-        var arr =[];
-        for (let index = 1; index <= this.max ; index++) {
+    range(star: number) {
+        var arr = [];
+        for (let index = 1; index <= this.max; index++) {
             arr.push(index);
         }
         return arr;
     }
-    selectStar(i:number){
+    selectStar(i: number) {
         this.rating = i;
     }
 }
